bf03a6e33f64643ee43f45b93e32b37673870d0b
Dragos <e.dragosmadalin@yahoo.com>
2017-04-03 19:09:53 +0300

    Made some changes

--- Assets/StatTaker.cs
+++ Assets/StatTaker.cs
@@ -3,41 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class StatTaker : MonoBehaviour {
+public class StatTaker : MonoBehaviour
+{
 
+    public GameObject targetedEnemy;
     // Use this for initialization
     private GameObject character = null;
+    private Base BaseCharacter = null;
     public Text HeroName;
+    public Text Message;
     public Image HPbar;
     public Image APbar;
     public Image ManaBar;
-    
+    public Button Ability1;
+    public Button Ability2;
+    public Button Ability3;
 
-	void Start () {
-		
-	}
+    //void Start()
+    //{
+     
+    //}
 
-    // Update is called once per frame
+
     void Update()
     {
         ///<summary>
         ///First i tried founding the object by using the RayCast
         ///didn't worked as planned but tomorrow i will fix this
         /// </summary>
+
         if (Input.GetMouseButtonDown(0) == true)
         {
             RaycastHit[] hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             hit = Physics.RaycastAll(ray);
-            foreach(RaycastHit hitObject in hit)
+            foreach (RaycastHit hitObject in hit)
             {
                 if (hitObject.transform.tag == "Hero")
                 {
                     character = hitObject.transform.gameObject;
+                    BaseCharacter = character.GetComponent<Base>();
                     break;
                 }
-                character = null;
-            }   
+               // character = null;
+               // BaseCharacter = null;
+            }
         }
 
 
@@ -49,10 +59,10 @@ public class StatTaker : MonoBehaviour {
             HPbar.enabled = true;
             APbar.enabled = true;
             ManaBar.enabled = true;
-            HeroName.text = character.GetComponent<Base>().ClassName;
-            HPbar.fillAmount = character.GetComponent<Base>().get_Hp / (character.GetComponent<Base>().stats.Stamina * Base.STAMINAMODIFIER);
-            APbar.fillAmount = character.GetComponent<Base>().get_ActionPoints / (Base.MAXACTIONPOINTS);
-            ManaBar.fillAmount = character.GetComponent<Base>().get_Mana / (character.GetComponent<Base>().stats.Endurance * Base.ENDURANCEMODIFIER);
+            HeroName.text = BaseCharacter.ClassName;
+            HPbar.fillAmount = BaseCharacter.get_Hp / (BaseCharacter.stats.Stamina * Base.STAMINAMODIFIER);
+            APbar.fillAmount = BaseCharacter.get_ActionPoints / (Base.MAXACTIONPOINTS);
+            ManaBar.fillAmount = BaseCharacter.get_Mana / (BaseCharacter.stats.Endurance * Base.ENDURANCEMODIFIER);
         }
         else
         {
@@ -61,9 +71,50 @@ public class StatTaker : MonoBehaviour {
             APbar.enabled = false;
             ManaBar.enabled = false;
         }
-       
+        Ability1.onClick.AddListener(() => CastAbility(BaseCharacter.abilities[0]));
+        Ability2.onClick.AddListener(() => CastAbility(BaseCharacter.abilities[1]));
+        Ability3.onClick.AddListener(() => CastAbility(BaseCharacter.abilities[2]));
+
+    }
 
 
+    void CastAbility(Power CastedPower)
+    {
+        if(CastedPower.NeededAP < BaseCharacter.get_Mana)
+        {
+            if (CastedPower.NeededAP < BaseCharacter.get_ActionPoints)
+            {
+                if (CastedPower.isAoe == false)
+                {
+                    Message.text = " ChooseTarget ";
 
+                    RaycastHit[] hit;
+                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                    hit = Physics.RaycastAll(ray);
+                    foreach (RaycastHit hitObject in hit)
+                    {
+                        if (hitObject.transform.tag == "Enemy")
+                        {
+                            targetedEnemy = hitObject.transform.gameObject;
+                            CastedPower.Action(BaseCharacter, new Base[] { targetedEnemy.GetComponent<Base>() });
+                            break;
+                        }
+                        // CastedPower.Action(BaseCharacter, TestEnemy);
+                    }
+                }
+                else
+                { // If it's AOE it should that all the targets automaticly 
+                }
+                  
+}
+            else
+            {
+                Message.text = "Not enough Mana";
+            }
+        }
+        else
+        {
+            Message.text = "Not enough Action Points";
+        }
     }
 }
